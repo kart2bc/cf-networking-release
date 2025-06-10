dd8733fa1558ff49a6628f7aac947eff965a2989
kart2bc <karthick.udayakumar@broadcom.com>
2025-06-10 13:57:28 -0400

    add github actions

--- .github/helpers/test.bash
+++ .github/helpers/test.bash
@@ -3,9 +3,10 @@
 set -eu
 set -o pipefail
 GH_WORKSPACE=$1
+BUILD_IMAGE=$2
 docker run --cap-add=SYS_ADMIN --rm --privileged \
 	-v "$GH_WORKSPACE:/workspace" \
 	-w /workspace \
 	-e MAPPING="$MAPPING" -e DB="$DB" -e DIR="$DIR" -e RUN_AS="$RUN_AS" -e VERIFICATIONS="$VERIFICATIONS" -e FUNCTIONS="$FUNCTIONS" \
-	us-central1-docker.pkg.dev/cf-diego-pivotal/tas-runtime-dockerhub-mirror/cloudfoundry/tas-runtime-mysql-5.7 \
+	"$BUILD_IMAGE" \
 	bash ./ci/shared/tasks/run-bin-test/task.bash --keep-going --trace -r --fail-on-pending --randomize-all --nodes=7 --race --timeout 30m --flake-attempts 2
